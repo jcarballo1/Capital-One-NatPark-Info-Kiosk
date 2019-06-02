cffe36b8f5d4b0004fbc8cc57c2e7164b9b8ae5f
Jennifer Carballo <jcarballo@smu.edu>
2019-06-02 04:00:22 -0500

    created files for education page

---
+++ src/java/org/mypackage/nationalpark/EdSearchResult.java
@@ -0,0 +1,15 @@
+package org.mypackage.nationalpark;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jcarb
+ */
+public class EdSearchResult {
+    
+}
